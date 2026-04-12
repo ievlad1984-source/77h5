930b63e72822a7661d1dd76eddfeeb89538350ec
ievlad1984-source <ievlad1984@gmail.com>
2026-04-12 16:00:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+app.use(express.static('public'));
 
 const app = express();
 const server = http.createServer(app);
