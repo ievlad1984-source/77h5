5de3e2b98f87bc8aee27cbe9b42eb8f287212103
ievlad1984-source <ievlad1984@gmail.com>
2026-04-13 09:55:09 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,21 +4,28 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
 
-const PORT = 3000;
-const boardsData = {}; // Тут зберігаються малюнки
+// ВАЖЛИВО: Render сам призначає порт. Якщо PORT немає, використовуємо 3000 для локалки.
+const PORT = process.env.PORT || 3000;
 
-// 1. Цей рядок дозволяє серверу бачити картинки або стилі, якщо вони у вас в окремих файлах
+const boardsData = {}; 
+
+// Раздача статики (текущая папка)
 app.use(express.static(__dirname));
 
-// 2. ГОЛОВНЕ: Обробка будь-якого ID дошки
-// Коли ви переходите на /some-id, сервер просто віддає ваш файл index.html
-app.get('/:boardId', (req, res) => {
+// Правило для главной страницы
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Обробка головної сторінки
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+// Правило для любой доски (динамический ID)
+// Используем регулярку, чтобы не перехватывать файлы типа style.css или script.js
+app.get('/:boardId', (req, res) => {
+    // Если в запросе есть точка (это файл, а не ID доски), пропускаем его
+    if (req.params.boardId.includes('.')) {
+        res.status(404).send('File not found');
+    } else {
+        res.sendFile(path.join(__dirname, 'index.html'));
+    }
 });
 
 io.on('connection', (socket) => {
@@ -29,6 +36,7 @@ io.on('connection', (socket) => {
     socket.emit('init-history', boardsData[boardId]);
 
     socket.on('new-object', (obj) => {
+        if (!boardsData[boardId]) boardsData[boardId] = [];
         boardsData[boardId].push(obj);
         socket.to(boardId).emit('new-object', obj);
     });
@@ -39,13 +47,14 @@ io.on('connection', (socket) => {
     });
 
     socket.on('undo', () => {
-        if (boardsData[boardId].length > 0) {
+        if (boardsData[boardId] && boardsData[boardId].length > 0) {
             boardsData[boardId].pop();
             io.in(boardId).emit('init-history', boardsData[boardId]);
         }
     });
 });
 
+// Слушаем динамический PORT
 http.listen(PORT, () => {
-    console.log(`Сервер працює: http://localhost:${PORT}`);
+    console.log(`Сервер запущен на порту ${PORT}`);
 });
