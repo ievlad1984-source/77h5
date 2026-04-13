600e5d1fae5a8a084b5ce2301a94fa08d099d741
ievlad1984-source <ievlad1984@gmail.com>
2026-04-13 09:53:00 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,50 +5,39 @@ const io = require('socket.io')(http);
 const path = require('path');
 
 const PORT = 3000;
+const boardsData = {}; // Тут зберігаються малюнки
 
-// Хранилище данных для досок (в памяти сервера)
-const boardsData = {};
+// 1. Цей рядок дозволяє серверу бачити картинки або стилі, якщо вони у вас в окремих файлах
+app.use(express.static(__dirname));
 
-// Раздача статических файлов (CSS, JS из папки public, если они есть)
-app.use(express.static('public'));
-
-// ГЛАВНОЕ ПРАВИЛО: Любой путь, кроме статики, отдает файл index.html
+// 2. ГОЛОВНЕ: Обробка будь-якого ID дошки
+// Коли ви переходите на /some-id, сервер просто віддає ваш файл index.html
 app.get('/:boardId', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Если зашли просто на корень / , тоже отдаем index.html
+// Обробка головної сторінки
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Работа с сокетами
 io.on('connection', (socket) => {
     const boardId = socket.handshake.query.boardId || 'main';
     socket.join(boardId);
 
-    // Создаем историю для доски, если её еще нет
-    if (!boardsData[boardId]) {
-        boardsData[boardId] = [];
-    }
-
-    // Отправляем историю новому пользователю
+    if (!boardsData[boardId]) boardsData[boardId] = [];
     socket.emit('init-history', boardsData[boardId]);
 
-    // Принимаем новый объект
     socket.on('new-object', (obj) => {
         boardsData[boardId].push(obj);
-        // Рассылаем всем в этой комнате, кроме отправителя
         socket.to(boardId).emit('new-object', obj);
     });
 
-    // Удаление доски
     socket.on('delete-board', (id) => {
         boardsData[id] = [];
         io.in(id).emit('board-deleted');
     });
 
-    // Отмена действия (Undo)
     socket.on('undo', () => {
         if (boardsData[boardId].length > 0) {
             boardsData[boardId].pop();
@@ -58,5 +47,5 @@ io.on('connection', (socket) => {
 });
 
 http.listen(PORT, () => {
-    console.log(`Сервер запущен: http://localhost:${PORT}`);
+    console.log(`Сервер працює: http://localhost:${PORT}`);
 });
