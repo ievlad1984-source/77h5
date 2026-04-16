6115e6ecdb86266ceb58421736e2c9d28ae1ecdc
ievlad1984-source <ievlad1984@gmail.com>
2026-04-16 11:25:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,63 +1,47 @@
 const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, { cors: { origin: "*" } });
 const path = require('path');
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const PORT = process.env.PORT || 3000;
+const boardsData = {}; 
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '11.html'));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
+app.get('/:boardId', (req, res) => {
+    if (req.params.boardId.includes('.')) return res.status(404).send('Not found');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
-const boards = new Map();
 
 io.on('connection', (socket) => {
-    console.log('Користувач підключився:', socket.id);
-    
-    socket.on('join-board', (boardId) => {
-        socket.join(boardId);
-        console.log(`Користувач ${socket.id} приєднався до дошки ${boardId}`);
-        
-        if (!boards.has(boardId)) {
-            boards.set(boardId, []);
-        }
-        
-        socket.emit('init-history', boards.get(boardId));
-    });
-    
-    socket.on('new-object', (obj) => {
-        const boardId = socket.handshake.query.boardId || 'default';
-        if (boards.has(boardId)) {
-            boards.get(boardId).push(obj);
-            socket.to(boardId).emit('new-object', obj);
-        }
-    });
-    
-    socket.on('update-all', (objects) => {
-        const boardId = socket.handshake.query.boardId || 'default';
-        if (boards.has(boardId)) {
-            boards.set(boardId, objects);
-            socket.to(boardId).emit('update-all', objects);
-        }
-    });
-    
-    socket.on('delete-board', () => {
-        const boardId = socket.handshake.query.boardId || 'default';
-        boards.set(boardId, []);
-        io.to(boardId).emit('board-deleted');
-    });
-    
-    socket.on('disconnect', () => {
-        console.log('Користувач відключився:', socket.id);
-    });
-});
+    const boardId = socket.handshake.query.boardId || 'main';
+    socket.join(boardId);
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`🚀 Сервер запущено на http://localhost:${PORT}`);
-    console.log(`📝 Відкрийте цю адресу в кількох вкладках для тестування`);
+    if (!boardsData[boardId]) boardsData[boardId] = [];
+    socket.emit('init-history', boardsData[boardId]);
+
+    socket.on('new-object', (obj) => {
+        boardsData[boardId].push(obj);
+        socket.to(boardId).emit('new-object', obj);
+    });
+
+    socket.on('update-all', (data) => {
+        boardsData[boardId] = data;
+        socket.to(boardId).emit('init-history', data);
+    });
+
+    socket.on('undo', () => {
+        if (boardsData[boardId]?.length > 0) {
+            boardsData[boardId].pop();
+            io.in(boardId).emit('init-history', boardsData[boardId]);
+        }
+    });
+
+    socket.on('delete-board', () => {
+        boardsData[boardId] = [];
+        io.in(boardId).emit('board-deleted');
+    });
 });
+http.listen(PORT, () => console.log(`Сервер запущен: порт ${PORT}`));
